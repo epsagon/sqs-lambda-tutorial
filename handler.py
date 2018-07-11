163494e04dcf57f2ce638bc4d08e153ed55340ef
ranrib <ran@epsagon.com>
2018-07-11 15:15:03 +0300

    Updating serverless.yml to include SQS resource.

--- handler.py
+++ handler.py
@@ -2,31 +2,31 @@
 Simple example for Lambda->SQS->Lambda.
 """
 
+import os
 import boto3
 
 SQS_CLIENT = boto3.client('sqs')
-SQS_URL = 'https://sqs.<region>.amazonaws.com/<account>/TutorialSQS'
 
 
-def start(_event, _context):
+def start(event, context):
     """
     First Lambda function. Triggered manually.
-    :param _event: AWS event data
-    :param _context: AWS function's context
+    :param event: AWS event data
+    :param context: AWS function's context
     :return: ''
     """
     print(SQS_CLIENT.send_message(
-        QueueUrl=SQS_URL,
+        QueueUrl=os.getenv('SQS_URL'),
         MessageBody='test'
     ))
     return ''
 
 
-def end(event, _context):
+def end(event, context):
     """
     Second Lambda function. Triggered by the SQS.
     :param event: AWS event data (this time will be the SQS's data)
-    :param _context: AWS function's context
+    :param context: AWS function's context
     :return: ''
     """
     print(event)
